31889189e2bdcc5f1a2db4846bc060e8f4017978
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-30 15:28:10 -0400

    finished lc_sub

--- l_c_sub_sum.rb
+++ l_c_sub_sum.rb
@@ -22,19 +22,22 @@ def largest_contiguous_subsum(list)
     current_sum = list.first
 
     list[1..-1].each do |ele|
-        # largest_sum = ele if ele > largest_sum
-        if current_sum + ele > largest_sum
-            largest_sum = current_sum + ele
+        largest_sum = ele if ele > largest_sum
+        
+        if current_sum + ele > largest_sum || current_sum + ele > 0
             current_sum += ele
+            largest_sum = current_sum
         else
-            current_sum += ele
             current_sum = 0
         end
+            
     end
 
     largest_sum    
 end
 
-list =[5, -3, 7, 10]
+list =[-5, -3, -10, -17]
+list2 =[2, 3, -6, 7, -6, 7]
 p largest_contiguous_subsum(list)
+p largest_contiguous_subsum(list2)
 
