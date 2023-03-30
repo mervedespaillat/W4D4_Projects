82f2424b890f99d7c3cc30d284a6e78d32a58400
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-03-30 17:20:34 -0400

    two_some created

---
+++ two_sum.rb
@@ -0,0 +1,20 @@
+
+
+#---------------------- Brute-force solution -----------------------------
+
+# def bad_two_sum?(arr, target_sum)
+
+#     arr.each_with_index do |ele, i1|
+#         arr.each_with_index do |ele2, i2|
+#             return true if i1 < i2 && ele + ele2 == target_sum
+#         end
+#     end
+#     false
+# end
+
+# arr = [0,1,5,7]
+# p bad_two_sum?(arr, 6)
+
+#---------------------- Sorting -----------------------------
+
+
